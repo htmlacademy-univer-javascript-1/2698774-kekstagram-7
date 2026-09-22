c894d50b51c6b7b3c32596c57cc1c6d9351847da
Keks <keks@htmlacademy.ru>
2026-09-22 14:36:41 +0300

    :heavy_check_mark: Сборка #4

---
+++ 4/js/functions.js
@@ -0,0 +1,35 @@
+// Функция для проверки длины строки
+function checkStringLength(string, maxLength) {
+  return string.length <= maxLength;
+}
+//console.log(checkStringLength('проверяемая строка', 10));
+checkStringLength('проверяемая строка', 10);
+
+function isPalindrome(string) {
+  const normalizedString = string.replaceAll(' ', '').toLowerCase();
+  let reversedString = '';
+  for (let i = normalizedString.length - 1; i >= 0; i--) {
+    reversedString += normalizedString[i];
+  }
+  return reversedString === normalizedString;
+}
+//console.log(isPalindrome('Лёша на полке клопа нашёл '));
+isPalindrome('Лёша на полке клопа нашёл ');
+
+function extractNumber(input) {
+  const string = input.toString();
+  let resultString = '';
+  for (let i = 0; i < string.length; i++) {
+    const char = string[i];
+    const digit = parseInt(char, 10);
+    if (!Number.isNaN(digit)) {
+      resultString += char;
+    }
+  }
+  if (resultString === '') {
+    return NaN;
+  }
+  return parseInt(resultString, 10);
+}
+extractNumber('1 кефир, 0.5 батона');
+//console.log(extractNumber('1 кефир, 0.5 батона')); // 105
---
+++ 4/js/main.js
@@ -0,0 +1,4 @@
+
+import { generatePhotos } from './data.js';
+
+generatePhotos();
